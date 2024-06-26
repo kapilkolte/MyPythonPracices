75431d2e12ca304205c1a654519e2c58f0963a2e
kapilkolte <138849466+kapilkolte@users.noreply.github.com>
2024-06-26 06:18:04 +0000

    Print-Statement Day1

---
+++ main.py
@@ -0,0 +1,14 @@
+print("My Name is Kapil")
+print("I am a student of class 12th")
+print("I am from India")
+print("I am a good boy")
+print("I am a good boy\nand a good student")
+print('K')
+print("k")
+print("0")
+
+print("She said: \"Hello\" and  then left.")
+
+print("A 'Single quote' inside a double quote")
+print("A 'double quote' inside a single quote")
+print("Alternatively you can just \"escape\" the quote")
